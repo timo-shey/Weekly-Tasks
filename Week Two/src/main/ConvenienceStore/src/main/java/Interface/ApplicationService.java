e05b3f4221e57cbc072eeadb0888046e169f045e
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2022-10-15 02:23:04 +0100

    Delete Week Two/src/main/ConvenienceStore directory

--- Week Two/src/main/ConvenienceStore/src/main/java/Interface/ApplicationService.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import CustomException.UsageException;
-import Model.Applicant;
-import Model.ConvenienceStore;
-
-public interface ApplicationService {
-    void apply(Applicant applicant, ConvenienceStore convenienceStore) throws UsageException;
-}
--- Week Two/src/main/ConvenienceStore/src/main/java/Model/Manager.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class Manager extends Staff {
-
-    public Manager(String firstName, String lastName, String gender, int age, int staffId, String department) {
-        super(firstName, lastName, gender, age, staffId, department);
-    }
-}
